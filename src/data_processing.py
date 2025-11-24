fcd1e4db5e0c0f626a1ea8ee417f579dfce5b785
Bruno Iorio <bruno.fernandes.iorio@gmail.com>
2025-11-24 12:27:35 +0100

    extra info

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,5 +1,6 @@
 import numpy as np 
-import pandas as pd
+import pandas as pd 
+from tqdm import tqdm
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
@@ -11,5 +12,31 @@ def chunking(text):
 def encode(text):
     return model.encode(text)
 
-def encode(list_of_text):
-    
+def encode_list(list_of_text):
+    other = list_of_text
+    for i in range(len(list_of_text)):
+        other[i] = encode(list_of_text[i])
+    return other
+
+def text_process_dataframe(df,columns):
+    df2 = df.copy()
+    for col in columns:
+        list_to_add = []
+        new_name = col + "_encoded"
+        for i in tqdm(range(len(df))):
+            text = df[col][i]
+            if isinstance(text,str):
+                chunked = chunking(text.lower())
+                list_of_encoded = encode_list(chunked)
+                list_to_add.append(list_of_encoded)
+            else:
+                list_to_add.append(np.nan)
+        df2[new_name] = list_to_add 
+    return df2
+                
+
+
+
+
+
+
