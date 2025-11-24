51356e017e3ec48c5e6a9f9876e62203adc14bb9
Bruno Iorio <bruno.fernandes.iorio@gmail.com>
2025-11-24 11:17:40 +0100

    corrected data with some analysis

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,4 +1,15 @@
 import numpy as np 
 import pandas as pd
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 
+
+def chunking(text):
+    return text.split(".")
+
+def encode(text):
+    return model.encode(text)
+
+def encode(list_of_text):
+    
